350452f85a338111e8d220f510304112fc7b10b5
Alexei A. Smekalkine <ikle@ikle.ru>
2022-08-21 02:16:49 +0300

    yonk-list-test: count request times

--- yonk-list-test.c
+++ yonk-list-test.c
@@ -7,19 +7,28 @@
  */
 
 #include <stdio.h>
+#include <time.h>
 
 #include <yonk/core.h>
 
 int main (int argc, char *argv[])
 {
 	struct yonk *o;
+	clock_t ts;
 
 	if ((o = yonk_alloc ("yonk-core.db", "r")) == NULL) {
 		fprintf (stderr, "E: cannot connect to database\n");
 		return 1;
 	}
 
+	ts = clock ();
+
 	yonk_list (o, 0, 0, stdout);
+
+	ts = clock () - ts;
+
+	printf ("I: requests completed in %.3f ms\n",
+		ts * (1000.0 / CLOCKS_PER_SEC));
 
 	yonk_free (o);
 	return 0;
